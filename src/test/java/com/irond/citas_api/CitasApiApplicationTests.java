7e74376c19d7ce0959c91242bfb90d48e82b5013
irond85 <69101982+irond85@users.noreply.github.com>
2025-04-01 18:00:58 -0500

    init project citas api

---
+++ src/test/java/com/irond/citas_api/CitasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irond.citas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
